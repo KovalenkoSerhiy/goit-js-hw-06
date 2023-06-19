39567647358331ed575165da02dd7e2630433b6b
KovalenkoSerhiy <118220875+KovalenkoSerhiy@users.noreply.github.com>
2023-06-20 00:47:56 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,27 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+const liEl = document.createElement("li");
+
+list.append(liEl);
+
+liEl.classList.add("item");
+
+liEl.innerHTML = ingredients.map((ingredient) => `${ingredient}`);
+
+console.log(list);
+
+
+
+
+
+// const listEl = ingredients.map((ingredient) => `<li class="item">${ingredient}</li>`).join("");
+
+// console.log(listEl);
+
+// list.innerHTML = listEl;
+
+
