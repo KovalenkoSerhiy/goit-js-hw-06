281cf8c73364dbac5fe17c495414aee8f7644960
KovalenkoSerhiy <118220875+KovalenkoSerhiy@users.noreply.github.com>
2023-07-10 23:56:41 +0300

    1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
-
+const formElement = document.querySelector('.login-form');
+formElement.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  const message = 'Всі поля повинні бути заповнені!';
+  const { email, password } = event.currentTarget.elements;
+  if (email.value === '' || password.value === '') {
+    alert(message);
+    return;
+  }
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+  console.dir(data);
+  formElement.reset();
+}
